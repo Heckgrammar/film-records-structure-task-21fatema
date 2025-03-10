f54097deafbaf890fdb0f2b365680970bcec572c
21fatema <144434558+21fatema@users.noreply.github.com>
2025-03-10 15:47:50 +0000

    Update Program.cs
    Hw

--- RecordsStructureTask1/Program.cs
+++ RecordsStructureTask1/Program.cs
@@ -28,12 +28,34 @@
             int position = 0;
 
             //  Write the code to do the following
+            
             // Loop through the array of films and check for the newest film
+            Film newestFilm = filmCollection[0];
+            foreach (Film film in filmCollection)
+            {
+                if (film.year > newestFilm.year)
+                {
+                    newestFilm = film;
+                }
+            }
+            
             // Produce one output to say the name of the newest film
+            Console.WriteLine("The newest film is: " + newestFilm.title);
 
             // Write the code to update the andMan record to show the film is currently being shown
 
+for (int i = 0; i < filmCollection.Length; i++)
+            {
+                if (filmCollection[i].title == "Ant-Man")
+                {
+                    filmCollection[i].beingShown = true;
+                    break;
+                }
+            }
 
+            Console.WriteLine("Ant-Man is now being shown: " + filmCollection[0].beingShown);
+
+            
         }
     }
 }
